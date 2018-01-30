49e57de924c0320e48f1cc53b32ec7736b3e7256
wanpeng <wanpeng19860919@163.com>
2018-01-30 15:33:17 +0800

    <feat>invite page

--- src/routes/Me/Invite.js
+++ src/routes/Me/Invite.js
@@ -4,13 +4,51 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link, Route, withRouter, Switch} from 'react-router-dom'
-import {Button, WingBlank, Toast} from 'antd-mobile'
+import {Button, WingBlank} from 'antd-mobile'
+import wx from 'tencent-wx-jssdk'
 import styles from './invite.module.scss'
+import {appStateAction, appStateSelector} from '../../utils/appstate'
+import {getMobileOperatingSystem} from '../../utils/OS'
 
 class Invite extends React.PureComponent {
   constructor(props) {
     document.title = "邀请代理"
     super(props)
+  }
+
+  componentWillMount() {
+    const {getJsApiConfig, entryURL} = this.props
+    const OS = getMobileOperatingSystem()
+    let jssdkURL = window.location.href
+    if(OS === 'iOS') {
+      //微信JS-SDK Bug: SPA(单页应用)ios系统必须使用首次加载的url初始化jssdk
+      jssdkURL = entryURL
+    }
+    getJsApiConfig({
+      debug: __DEV__? true: true,
+      jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'].toString(),
+      url: jssdkURL,
+      success: this.getJsApiConfigSuccess,
+      error: (error) => {console.log(error)}
+    })
+  }
+
+  getJsApiConfigSuccess = (configInfo) => {
+    wx.config(configInfo)
+  }
+
+  onShare = () => {
+    wx.onMenuShareTimeline({
+      title: '小吉互动',
+      link: 'http://vote.xiaojee.cn',
+      imgUrl: 'http://ac-l3cae9l7.clouddn.com/9b8514cffb7b4d9e2395.png',
+      success: function (res) {
+
+      },
+      cancel: function (res) {
+
+      }
+    })
   }
 
   render() {
@@ -20,7 +58,7 @@ class Invite extends React.PureComponent {
           <img className={styles.img} src="http://ac-l3cae9l7.clouddn.com/9b8514cffb7b4d9e2395.png" alt=""/>
         </div>
         <WingBlank style={{marginTop: '50px'}}>
-          <Button type="primary">分享给好友</Button>
+          <Button type="primary" onClick={this.onShare}>分享给好友</Button>
         </WingBlank>
       </div>
     )
@@ -29,10 +67,12 @@ class Invite extends React.PureComponent {
 
 const mapStateToProps = (state, ownProps) => {
   return {
+    entryURL: appStateSelector.selectEntryURL(state)
   }
 }
 
 const mapDispatchToProps = {
+  ...appStateAction,
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Invite))
